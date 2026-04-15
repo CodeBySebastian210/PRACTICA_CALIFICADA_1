bdc5783b0ea1414b0ae3dbe9c3866a6972b44534
CodeBySebastian210 <forceunited100@gmail.com>
2026-04-14 22:44:50 -0500

    Add files via upload

---
+++ PRACTICA/SEMANA_4/src/Actividad.java
@@ -0,0 +1,14 @@
+
+public class Actividad {
+    public static void main(String[] args) {
+        int A[][] = {
+            {1, 2},
+            {3, 4}
+        };
+        int triangular[][] = {
+            {1, 0, 0},
+            {2, 3, 0},
+            {4, 5, 6}
+        };
+    }   
+}
