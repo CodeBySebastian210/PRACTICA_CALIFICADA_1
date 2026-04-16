4f10ff8ef24d33703605816ae8b31f46d1da13ee
CodeBySebastian210 <forceunited100@gmail.com>
2026-04-15 23:28:04 -0500

    Add files via upload

--- PRACTICA/SEMANA_4/src/Actividad.java
+++ PRACTICA/SEMANA_4/src/Actividad.java
@@ -10,5 +10,13 @@ public class Actividad {
             {2, 3, 0},
             {4, 5, 6}
         };
+        
+        System.out.println("Matriz triangular (solo parte útil):");
+        for (int i = 0; i < triangular.length; i++) {
+            for (int j = 0; j <= i; j++) { // solo parte inferior
+                System.out.print(triangular[i][j] + " ");
+            }
+            System.out.println();
+        }        
     }   
 }
